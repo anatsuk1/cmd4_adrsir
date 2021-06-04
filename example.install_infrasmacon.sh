eae92caebd2b9309626d34bb59daf9d847fdf150
anatsuk1 <anatsuk1@anau.net>
2021-06-04 23:17:02 +0900

    [Update] New Support of Raspberry Pi Zero WH

--- example.install_infrasmacon.sh
+++ example.install_infrasmacon.sh
@@ -11,12 +11,12 @@ chown -R pi:pi /var/opt/infrasmacon
 echo "  Copying 'config.json' file to /var/lib/homebridge/"
 cp ./config.json /var/lib/homebridge/
 
-echo "  Replacing sensitive infomation in /var/lib/homebridge/config.json"
+echo "  Replacing dyson-tag to sensitive information in config.json"
 sed -i -e 's/DYSON-IP/XXX.XXX.XXX.XXX/g' /var/lib/homebridge/config.json
 sed -i -e 's/DYSON-SERIALNUMBER/XXXXX-XXX-XX-XXXXXXXX-XXX/g' /var/lib/homebridge/config.json
 sed -i -e 's/DYSON-PASSWORD/XXXXXXXXXXXXXXXX/g' /var/lib/homebridge/config.json
 
-echo "  Replacing sensitive infomation in /var/lib/homebridge/config.json"
+echo "  Replacing hue-tag to sensitive information in config.json"
 sed -i -e 's/HUE-ATTRIBUTE/XXXXXXXXXXXXXXXX/g' /var/lib/homebridge/config.json
 sed -i -e 's/HUE-VALUE/XXXXXXXXXX/g' /var/lib/homebridge/config.json
 
